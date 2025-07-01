4af44b329aa8b5cccdb771a9f6fc8d651b783636
Pramananda Sarkar <pramanandasarkar02@gmail.com>
2025-07-02 00:13:04 +0600

    all searvices created

--- info-pulse-api/news-processing-service/src/main.ts
+++ info-pulse-api/news-processing-service/src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
   
-  await app.listen(3001);
+  await app.listen(3002);
 }
 bootstrap();
